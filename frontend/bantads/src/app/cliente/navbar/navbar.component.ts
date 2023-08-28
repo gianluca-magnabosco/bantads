dd8fc1667b3aec341828e03fc5682ee4a17cc2d0
Guilherme <101355723+guilhermelike@users.noreply.github.com>
2023-08-28 11:47:21 -0300

    rotas de cliente e navbar adicionadas

--- frontend/bantads/src/app/cliente/navbar/navbar.component.ts
+++ frontend/bantads/src/app/cliente/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'cliente-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule]
+  imports: [MatToolbarModule, MatButtonModule, RouterModule]
 })
 export class NavbarComponent {
 
