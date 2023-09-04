085b626032e5352c88fa6a25bf7255c69bac1f17
vinicius-ratzke <vinicius_servelo@hotmail.com>
2023-09-04 11:35:43 -0300

    ordenação gerente/inicio

--- frontend/bantads/src/app/gerente/inicio/inicio.component.ts
+++ frontend/bantads/src/app/gerente/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { GerenteService } from '../services/gerente.service';
 
+interface Pedido {
+  nome: string;
+  cpf: string;
+  salario: number;
+}
+
 @Component({
   selector: 'gerente-inicio',
   templateUrl: './inicio.component.html',
@@ -8,15 +14,44 @@ import { GerenteService } from '../services/gerente.service';
 })
 export class InicioComponent implements OnInit {
 
-  private pedidosPendentesAprovacao: any;
+  private pedidosPendentesAprovacao: Pedido[];
+  private ordenacaoAtual: string = '';
+  private ordemCrescente: boolean = true; 
 
-  constructor(private gerenteService: GerenteService) { }
+  constructor(private gerenteService: GerenteService) {
+    this.pedidosPendentesAprovacao = [];
+  }
 
   ngOnInit(): void {
     this.pedidosPendentesAprovacao = this.gerenteService.getPedidosPendentesAprovacao();
   }
 
   get listaPedidosPendentesAprovacao() {
-    return this.pedidosPendentesAprovacao;
+    const listaOrdenada = [...this.pedidosPendentesAprovacao]; 
+    if (this.ordenacaoAtual === 'nome') {
+      listaOrdenada.sort((a: Pedido, b: Pedido) => a.nome.localeCompare(b.nome));
+    } else if (this.ordenacaoAtual === 'cpf') {
+      listaOrdenada.sort((a: Pedido, b: Pedido) => a.cpf.localeCompare(b.cpf));
+    } else if (this.ordenacaoAtual === 'salario') {
+      listaOrdenada.sort((a: Pedido, b: Pedido) => a.salario - b.salario);
+    }
+
+    if (!this.ordemCrescente) {
+      listaOrdenada.reverse();
+    }
+
+    return listaOrdenada;
+  }
+
+
+  ordenarLista(atributo: string) {
+    if (this.ordenacaoAtual === atributo) {
+
+      this.ordemCrescente = !this.ordemCrescente;
+    } else {
+
+      this.ordemCrescente = true;
+    }
+    this.ordenacaoAtual = atributo;
   }
 }
--- frontend/bantads/src/app/gerente/services/gerente.service.ts
+++ frontend/bantads/src/app/gerente/services/gerente.service.ts
@@ -10,11 +10,11 @@ export class GerenteService {
   getPedidosPendentesAprovacao(): any {
     return (
       [
-        { id: "1", nome: "Amigao", cpf: "111.111.111-11", salario: "R$ 1500,00" },
-        { id: "2", nome: "Amigao", cpf: "111.111.111-11", salario: "R$ 1500,00" },
-        { id: "3", nome: "Amigao", cpf: "111.111.111-11", salario: "R$ 1500,00" },
-        { id: "4", nome: "Amigao", cpf: "111.111.111-11", salario: "R$ 1500,00" },
-        { id: "5", nome: "Amigao", cpf: "111.111.111-11", salario: "R$ 1500,00" },
+        { id: "1", nome: "Alef Manga", cpf: "837.456.721-21", salario: "R$ 12000,00" },
+        { id: "2", nome: "Marina Silva", cpf: "164.285.129-65", salario: "R$ 5000,32" },
+        { id: "3", nome: "Zé da Manga", cpf: "194.347.285-92", salario: "R$ 1254,43" },
+        { id: "4", nome: "James da Salada de Fruta", cpf: "873.831.762-67", salario: "R$ 950,21" },
+        { id: "5", nome: "Bluezao", cpf: "012.845.786-72", salario: "R$ 10,01" },
       ]
     )
   }
