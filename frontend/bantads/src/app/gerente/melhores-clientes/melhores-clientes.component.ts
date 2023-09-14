ab44a5dd5b82b901e1eb210f7904a7ccd0ba92c0
Guilherme <101355723+guilhermelike@users.noreply.github.com>
2023-09-13 21:39:54 -0300

    Adicionado botões de ordenação no melhores clientes

--- frontend/bantads/src/app/gerente/melhores-clientes/melhores-clientes.component.ts
+++ frontend/bantads/src/app/gerente/melhores-clientes/melhores-clientes.component.ts
@@ -22,6 +22,31 @@ export class MelhoresClientesComponent {
     this.filteredClientes.sort((a, b) => (b.saldo ?? 0) - (a.saldo ?? 0));
   }
   
+  sortKey: keyof ClienteConsultaListagem = "nome";
+  sortAsc = false;
+
+  sortData(key: keyof ClienteConsultaListagem = this.sortKey): void {
+    if (key === this.sortKey) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortAsc = true;
+      this.sortKey = key;
+    }
+
+    this.melhoresClientes.sort((a, b) => {
+      const valA = a[key];
+      const valB = b[key];
+
+      if (typeof valA === 'string' && typeof valB === 'string') {
+        return this.sortAsc ? valA.localeCompare(valB) : valB.localeCompare(valA);
+      } else if (typeof valA === 'number' && typeof valB === 'number') {
+        return this.sortAsc ? valA - valB : valB - valA;
+      } else {
+        return 0;
+      }
+    });
+  }
+
   
 
 
