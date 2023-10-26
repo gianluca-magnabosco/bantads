ef0adef5e8ec74947948c152b69f59e000cf99fd
Guilherme <101355723+guilhermelike@users.noreply.github.com>
2023-10-25 23:09:07 -0300

    db_gerente fix

---
+++ backend/gerente/src/main/java/br/ufpr/tads/dac/gerente/GerenteApplication.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.dac.gerente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenteApplication.class, args);
+	}
+
+}
---
+++ backend/gerente/src/test/java/br/ufpr/tads/dac/gerente/GerenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufpr.tads.dac.gerente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
